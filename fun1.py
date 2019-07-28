3c2fdda9976fa5bbe0898d2fcda01ee274e3c3ee
robert21-meet <robert21@meet.mit.edu>
2019-07-28 13:58:18 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,12 @@
+def pluscalc(start,end):
+    total=0
+    for i in range(start,end+1):
+        total=total+i
+    print (total)
+pluscalc(1,2)
+pluscalc(1,100)
+pluscalc(1000,5000)
+
+
+
+        
